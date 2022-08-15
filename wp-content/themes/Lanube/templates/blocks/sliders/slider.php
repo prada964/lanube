69f20cb49c129f00f7da34213c3e3faf786db3c1
xsebaslds <setianbas@hotmail.com>
2022-08-15 13:14:26 -0500

    Comprobando cambios

--- wp-content/themes/Lanube/templates/blocks/sliders/slider.php
+++ wp-content/themes/Lanube/templates/blocks/sliders/slider.php
@@ -2,7 +2,7 @@
 $galery_images = get_field('galeria_de_imagenes');
 ?>
 <div class="w-100 col-12 p-0 m-0" style="background: #C9C9C9;">
-    <div class="lanube-slider-clientes py-5">
+    <div class="lanube-slider-clientes py-4">
         <?php 
         if (is_array($galery_images) || is_object($galery_images)):
         foreach($galery_images as $slide):?>
